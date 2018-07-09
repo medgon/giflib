e03dfab0ce0af0265004c4aec201e32d33ffba0f
medgon <minhtly1@gmail.com>
2018-07-08 22:59:03 -0400

    Initial commit

---
+++ src/main/java/com/dyheart/giflib/controller/GifController.java
@@ -0,0 +1,15 @@
+package com.dyheart.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+
+    @RequestMapping(value = "/")
+    public String listGifs(){
+        return "home";
+    }
+
+}
